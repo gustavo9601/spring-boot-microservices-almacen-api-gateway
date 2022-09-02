ec28ece7dc221e1c39db95e03bb0018293defb47
gustavo9601 <tavo9601@gmail.com>
2022-09-02 00:02:06 -0500

    add api gateway

---
+++ src/main/java/com/gustavomp/almacen/springbootmicroservicesalmacenapigateway/SpringBootMicroservicesAlmacenApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomp.almacen.springbootmicroservicesalmacenapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesAlmacenApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesAlmacenApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavomp/almacen/springbootmicroservicesalmacenapigateway/SpringBootMicroservicesAlmacenApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomp.almacen.springbootmicroservicesalmacenapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesAlmacenApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
